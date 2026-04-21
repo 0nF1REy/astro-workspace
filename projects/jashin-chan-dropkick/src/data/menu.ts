a25e9380c55a7fbe4249dba4da144e66751426fa
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-04-21 00:28:47 -0300

    main: feat (jashin-chan-dropkick/hero,menu) - Switched hero navigation to image-based buttons
    - Replaced text-based navigation with image buttons in the hero section, updated menu data to use image paths, and added all required navigation images.
    - Improved hover effects and refactored related styles for a more visual navigation experience.

--- projects/jashin-chan-dropkick/src/data/menu.ts
+++ projects/jashin-chan-dropkick/src/data/menu.ts
@@ -1,18 +1,59 @@
 export interface MenuItem {
-  href: string;
+  href?: string;
   id: string;
-  label: string;
+  img: string;
+  imgHover: string;
 }
 
 export const menuItems: MenuItem[] = [
-  { href: "/news", id: "jd-news", label: "News" },
-  { href: "/on-air", id: "jd-onair", label: "On Air" },
-  { href: "/staffcast", id: "jd-staffcast", label: "Staff/Cast" },
-  { href: "/story", id: "jd-story", label: "Story" },
-  { href: "/character", id: "jd-character", label: "Character" },
-  { href: "/disc", id: "jd-disc", label: "Disc" },
-  { href: "/goods", id: "jd-goods", label: "Goods" },
-  { href: "/movie", id: "jd-movie", label: "Movie" },
-  { href: "/special", id: "jd-special", label: "Special" },
-    { href: "/business", id: "jd-business", label: "Business Info" },
+  {
+    id: "nav_news",
+    img: "/assets/images/home/hero/nav/off/nav_news_off.png",
+    imgHover: "/assets/images/home/hero/nav/on/nav_news_on.png",
+  },
+  {
+    id: "nav_onair",
+    img: "/assets/images/home/hero/nav/off/nav_onair_off.png",
+    imgHover: "/assets/images/home/hero/nav/on/nav_onair_on.png",
+  },
+  {
+    id: "nav_staffcast",
+    img: "/assets/images/home/hero/nav/off/nav_staffcast_off.png",
+    imgHover: "/assets/images/home/hero/nav/on/nav_staffcast_on.png",
+  },
+  {
+    id: "nav_story",
+    img: "/assets/images/home/hero/nav/off/nav_story_off.png",
+    imgHover: "/assets/images/home/hero/nav/on/nav_story_on.png",
+  },
+  {
+    id: "nav_character",
+    img: "/assets/images/home/hero/nav/off/nav_character_off.png",
+    imgHover: "/assets/images/home/hero/nav/on/nav_character_on.png",
+  },
+  {
+    id: "nav_cddvdbd",
+    img: "/assets/images/home/hero/nav/off/nav_cddvdbd_off.png",
+    imgHover: "/assets/images/home/hero/nav/on/nav_cddvdbd_on.png",
+  },
+  {
+    id: "nav_goods",
+    img: "/assets/images/home/hero/nav/off/nav_goods_off.png",
+    imgHover: "/assets/images/home/hero/nav/on/nav_goods_on.png",
+  },
+  {
+    id: "nav_movie",
+    img: "/assets/images/home/hero/nav/off/nav_movie_off.png",
+    imgHover: "/assets/images/home/hero/nav/on/nav_movie_on.png",
+  },
+  {
+    id: "nav_special",
+    img: "/assets/images/home/hero/nav/off/nav_special_off.png",
+    imgHover: "/assets/images/home/hero/nav/on/nav_special_on.png",
+  },
+  {
+    id: "nav_business-info",
+    img: "/assets/images/home/hero/nav/off/nav_business-info_off.png",
+    imgHover: "/assets/images/home/hero/nav/on/nav_business-info_on.png",
+  },
 ];
