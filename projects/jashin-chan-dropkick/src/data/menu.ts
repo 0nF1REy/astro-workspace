8e8a7dc764939004c44c18acea7e0dafab86f51b
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-04-27 18:48:45 -0300

    feature/mobile-responsiveness: feat (jashin-chan-dropkick/header,menu,assets) - Added header navbar images and switch Header to use secondary nav assets for improved mobile responsiveness
    - Added new header navbar image assets and extended the menu data to include secondaryImg.
    - Updated the Header component to use these secondary images and adjusted layout/spacing for better mobile/desktop navigation behavior.

--- projects/jashin-chan-dropkick/src/data/menu.ts
+++ projects/jashin-chan-dropkick/src/data/menu.ts
@@ -3,6 +3,7 @@ export interface MenuItem {
   id: string;
   img: string;
   imgHover: string;
+  secondaryImg: string;
 }
 
 export const menuItems: MenuItem[] = [
@@ -11,59 +12,69 @@ export const menuItems: MenuItem[] = [
     href: "/news",
     img: "/assets/images/home/hero/nav/off/nav_news_off.png",
     imgHover: "/assets/images/home/hero/nav/on/nav_news_on.png",
+    secondaryImg: "/assets/images/header/nav/navbar_news.png",
   },
   {
     id: "nav_onair",
     href: "/onair",
     img: "/assets/images/home/hero/nav/off/nav_onair_off.png",
     imgHover: "/assets/images/home/hero/nav/on/nav_onair_on.png",
+    secondaryImg: "/assets/images/header/nav/navbar_onair.png",
   },
   {
     id: "nav_staffcast",
     href: "/staffcast",
     img: "/assets/images/home/hero/nav/off/nav_staffcast_off.png",
     imgHover: "/assets/images/home/hero/nav/on/nav_staffcast_on.png",
+    secondaryImg: "/assets/images/header/nav/navbar_staffandcast.png",
   },
   {
     id: "nav_story",
     href: "/story",
     img: "/assets/images/home/hero/nav/off/nav_story_off.png",
     imgHover: "/assets/images/home/hero/nav/on/nav_story_on.png",
+    secondaryImg: "/assets/images/header/nav/navbar_story.png",
   },
   {
     id: "nav_character",
     href: "/character",
     img: "/assets/images/home/hero/nav/off/nav_character_off.png",
     imgHover: "/assets/images/home/hero/nav/on/nav_character_on.png",
+    secondaryImg: "/assets/images/header/nav/navbar_character.png",
   },
   {
     id: "nav_cddvdbd",
     href: "/disc",
     img: "/assets/images/home/hero/nav/off/nav_cddvdbd_off.png",
     imgHover: "/assets/images/home/hero/nav/on/nav_cddvdbd_on.png",
+    secondaryImg: "/assets/images/header/nav/navbar_disc.png",
   },
   {
     id: "nav_goods",
     href: "/goods",
     img: "/assets/images/home/hero/nav/off/nav_goods_off.png",
     imgHover: "/assets/images/home/hero/nav/on/nav_goods_on.png",
+    secondaryImg: "/assets/images/header/nav/navbar_goods.png",
   },
   {
     id: "nav_movie",
     href: "/movie",
     img: "/assets/images/home/hero/nav/off/nav_movie_off.png",
     imgHover: "/assets/images/home/hero/nav/on/nav_movie_on.png",
+    secondaryImg: "/assets/images/header/nav/navbar_movie.png",
   },
   {
     id: "nav_special",
     href: "/special",
     img: "/assets/images/home/hero/nav/off/nav_special_off.png",
     imgHover: "/assets/images/home/hero/nav/on/nav_special_on.png",
+    secondaryImg: "/assets/images/header/nav/navbar_special.png",
   },
   {
     id: "nav_business-info",
     href: "/business-info",
     img: "/assets/images/home/hero/nav/off/nav_business-info_off.png",
     imgHover: "/assets/images/home/hero/nav/on/nav_business-info_on.png",
+    secondaryImg: "/assets/images/header/nav/navbar_business-info.png",
   },
 ];
