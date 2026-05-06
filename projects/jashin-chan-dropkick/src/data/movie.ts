02b687ce98931ff9ea860f33bcf619352aab967d
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-05-06 14:35:34 -0300

    feature/movie-page: feat (jashin-chan-dropkick/movie) - Added Movie page with List section and title assets

---
+++ projects/jashin-chan-dropkick/src/data/movie.ts
@@ -0,0 +1,7 @@
+export const movieAssets = {
+  bgPattern: `url("/assets/images/global/bg_pattern.png")`,
+  bgGirl: `url("/assets/images/global/bg_character01.png")`,
+  bgWave: `url("/assets/images/global/bg_wave_bottom.png")`,
+  ttlMovie: "/assets/images/movie/list/ttl_movie.png",
+  ttlSpMovie: "/assets/images/movie/list/ttl_sp_movie.png",
+} as const;
