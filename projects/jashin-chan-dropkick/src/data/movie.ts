b527da4e0b2984e74dfea30646c3c1b9f396ab8d
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-05-07 17:01:30 -0300

    feature/movie-page: feat (jashin-chan-dropkick/movie) - Added movieVideos data and responsive video embeds
    - Introduced a movieVideos array and renders responsive YouTube iframes on the Movie page, including layout and style updated for desktop and mobile.

--- projects/jashin-chan-dropkick/src/data/movie.ts
+++ projects/jashin-chan-dropkick/src/data/movie.ts
@@ -5,3 +5,13 @@ export const movieAssets = {
   ttlMovie: "/assets/images/movie/list/ttl_movie.png",
   ttlSpMovie: "/assets/images/movie/list/ttl_sp_movie.png",
 } as const;
+
+export const movieVideos = [
+  "https://www.youtube.com/embed/R3_7vqdslZk",
+  "https://www.youtube.com/embed/fUMqiEIkbBQ",
+  "https://www.youtube.com/embed/Vjryf4u--Ww",
+  "https://www.youtube.com/embed/K45goZa5yaM",
+  "https://www.youtube.com/embed/7WL3sT8Wepw",
+  "https://www.youtube.com/embed/zuUFl0zMI94",
+  "https://www.youtube.com/embed/xx70T2tgrwg",
+];
