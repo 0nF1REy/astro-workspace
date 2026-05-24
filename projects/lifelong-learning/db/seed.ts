bb8aaca58607f7084879917834c9b8c6591294ff
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-05-24 18:58:19 -0300

    lifelong-learning: chore (lifelong-learning/scripts,seed) - Reorganized npm scripts for clarity and marked seed anime data with local development indicators

--- projects/lifelong-learning/db/seed.ts
+++ projects/lifelong-learning/db/seed.ts
@@ -4,21 +4,21 @@ import { Links, db } from "astro:db";
 export default async function seed() {
   await db.insert(Links).values([
     {
-      title: "Jashin-chan Dropkick",
+      title: "Jashin-chan Dropkick — [LOCAL]",
       url: "https://jashinchan.com/",
       date: new Date(),
       isRead: true,
       upvoteNum: 3,
     },
     {
-      title: "Kill la Kill",
+      title: "Kill la Kill — [LOCAL]",
       url: "https://www.kill-la-kill.jp/",
       date: new Date(),
       isRead: false,
       upvoteNum: 10000,
     },
     {
-      title: "Fire Force",
+      title: "Fire Force — [LOCAL]",
       url: "https://fireforce-anime.jp/",
       date: new Date(),
       isRead: true,
