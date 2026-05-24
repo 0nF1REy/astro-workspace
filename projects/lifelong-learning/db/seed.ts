1fa9228718d516f30a4ac8588d413298683d74c3
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-05-24 16:39:46 -0300

    lifelong-learning: feat (lifelong-learning/db,seed) - Implemented Links seed with anime records and added NOW default for date column

--- projects/lifelong-learning/db/seed.ts
+++ projects/lifelong-learning/db/seed.ts
@@ -1,6 +1,28 @@
-import { db } from "astro:db";
+import { Links, db } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-  // TODO
+  await db.insert(Links).values([
+    {
+      title: "Jashin-chan Dropkick",
+      url: "https://jashinchan.com/",
+      date: new Date(),
+      isRead: true,
+      upvoteNum: 3,
+    },
+    {
+      title: "Kill la Kill",
+      url: "https://www.kill-la-kill.jp/",
+      date: new Date(),
+      isRead: false,
+      upvoteNum: 10000,
+    },
+    {
+      title: "Fire Force",
+      url: "https://fireforce-anime.jp/",
+      date: new Date(),
+      isRead: true,
+      upvoteNum: 1000,
+    },
+  ]);
 }
