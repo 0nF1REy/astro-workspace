3699678aad411c504471d4313a922c932346177d
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-05-28 17:20:17 -0300

    main: chore (lifelong-learning/tooling) - Integrated ESLint and Prettier with TypeScript and Astro plugin support

---
+++ projects/lifelong-learning/eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import astro from "eslint-plugin-astro";
+import globals from "globals";
+
+export default [
+  {
+    ignores: ["dist/**", ".astro/**", ".vercel/**", "node_modules/**"],
+  },
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  ...astro.configs.recommended,
+
+  {
+    files: ["**/*.{js,mjs,cjs,ts,astro}"],
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+];
