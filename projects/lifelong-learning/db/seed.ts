a09086da1ea71c3a4d439dd4db19621317a428cc
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-05-30 18:03:23 -0300

    main: feat (lifelong-learning/links) - Added link edit workflow in dialog and enrich seed link metadata

--- projects/lifelong-learning/db/seed.ts
+++ projects/lifelong-learning/db/seed.ts
@@ -5,20 +5,23 @@ export default async function seed() {
   await db.insert(Links).values([
     {
       title: "Jashin-chan Dropkick — [LOCAL]",
-      description: "This is a sample description",
-      url: "https://jashinchan.com/",
+      description:
+        "Site oficial do anime Jashin-chan Dropkick, com notícias, personagens, episódios, vídeos promocionais, eventos e informações da franquia.",
+      url: "https://jashinchan.com",
       isRead: true,
     },
     {
       title: "Kill la Kill — [LOCAL]",
-      description: "This is a sample description",
-      url: "https://www.kill-la-kill.jp/",
+      description:
+        "Site oficial do anime Kill la Kill, produzido pelo Studio Trigger, contendo informações sobre a história, personagens, equipe, produtos e conteúdos promocionais.",
+      url: "https://www.kill-la-kill.jp",
       isRead: false,
     },
     {
       title: "Fire Force — [LOCAL]",
-      description: "This is a sample description",
-      url: "https://fireforce-anime.jp/",
+      description:
+        "Site oficial do anime Fire Force (Enen no Shouboutai), com detalhes sobre temporadas, personagens, notícias, trailers e materiais relacionados à série.",
+      url: "https://fireforce-anime.jp",
       isRead: true,
     },
   ]);
