520a286487223ad026a7378ce3e85bf8ebe29ae7
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-06-01 00:05:17 -0300

    main: feat (lifelong-learning/middleware) - Implemented feature module for middleware with context locals support and demonstration page

--- projects/lifelong-learning/src/middleware/index.ts
+++ projects/lifelong-learning/src/middleware/index.ts
@@ -1,6 +1,12 @@
 import { defineMiddleware } from "astro:middleware";
 
-export const onRequest = defineMiddleware(async ({ url }, next) => {
+export const onRequest = defineMiddleware(async (context, next) => {
+  const { url } = context;
+
+  context.locals.name = "John Wick";
+
+  console.log("[Middleware] Dados adicionados à requisição:", context.locals);
+
   const redirects = ["/"];
 
   if (redirects.includes(url.pathname)) {
