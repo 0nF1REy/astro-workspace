67769105666e9950ea768066321a0ba687911ed9
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-06-06 11:21:07 -0300

    main: feat (lifelong-learning/pokemon) - Initialized module foundation with pagination constants and robust TypeScript definitions

---
+++ projects/lifelong-learning/src/lib/pokemon/pagination.ts
@@ -0,0 +1,2 @@
+export const POKEMON_TOTAL_ITEMS = 151;
+export const POKEMON_PAGE_SIZE = 20;
--- projects/lifelong-learning/src/types/PokeType.ts
+++ projects/lifelong-learning/src/types/PokeType.ts
@@ -1,11 +1,34 @@
-type Pokemon = {
+export interface Pokemon {
+  id: number;
   name: string;
-  url: string;
-};
+  image: string;
+  types: string[];
+  height: number;
+  weight: number;
+}
 
-export type PokemonRes = {
-  count: number;
-  next: string | null;
-  previous: string | null;
-  results: Pokemon[];
-};
+export interface PokemonListRes {
+  results: Array<{
+    name: string;
+    url: string;
+  }>;
+}
+
+export interface PokemonDetailRes {
+  id: number;
+  name: string;
+  sprites: {
+    other: {
+      "official-artwork": {
+        front_default: string;
+      };
+    };
+  };
+  types: Array<{
+    type: {
+      name: string;
+    };
+  }>;
+  height: number;
+  weight: number;
+}
