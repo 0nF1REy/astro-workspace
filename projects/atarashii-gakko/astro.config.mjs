ddbdc93e820395d954fb766fb0b0e4e411039734
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-06-22 23:07:30 -0300

    main: chore (atarashii-gakko/deps,tooling) - Upgraded project to Astro 7 and streamline development tooling

--- projects/atarashii-gakko/astro.config.mjs
+++ projects/atarashii-gakko/astro.config.mjs
@@ -3,5 +3,19 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  // Site e roteamento
+  site: "https://atarashii-gakko.vercel.app",
   output: "static",
+  trailingSlash: "never",
+
+  // Servidor de desenvolvimento
+  server: {
+    host: true,
+    port: 4321,
+  },
+
+  // Dev Toolbar
+  devToolbar: {
+    enabled: false,
+  },
 });
