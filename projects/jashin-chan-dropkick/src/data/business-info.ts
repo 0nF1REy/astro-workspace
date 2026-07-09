aa5623cd87c5edf7fd4ccd3f4c70b898fe473170
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-07-08 22:09:50 -0300

    main: refactor (jashin-chan-dropkick/business-info) - Removed redundant asset data module

--- projects/jashin-chan-dropkick/src/data/business-info.ts
+++
@@ -1,5 +0,0 @@
-export const businessInfoAssets = {
-  ttlBusinessInfo: "/assets/images/business-info/info/ttl_business-info.png",
-  ttlSpBusinessInfo:
-    "/assets/images/business-info/info/ttl_sp_business-info.png",
-} as const;
