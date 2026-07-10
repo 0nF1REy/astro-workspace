2a0711392399fc5a256ff77faa377841650a5a24
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-07-09 21:17:22 -0300

    main: refactor (jashin-chan-dropkick/movie-page) - Migrated movie page to shared info shell

--- projects/jashin-chan-dropkick/src/data/movie.ts
+++ projects/jashin-chan-dropkick/src/data/movie.ts
@@ -1,11 +1,3 @@
-export const movieAssets = {
-  bgPattern: `url("/assets/images/global/bg_pattern.png")`,
-  bgGirl: `url("/assets/images/global/bg_character01.png")`,
-  bgWave: `url("/assets/images/global/bg_wave_bottom.png")`,
-  ttlMovie: "/assets/images/movie/list/ttl_movie.png",
-  ttlSpMovie: "/assets/images/movie/list/ttl_sp_movie.png",
-} as const;
-
 export const movieVideos: string[] = [
   "https://www.youtube.com/embed/R3_7vqdslZk",
   "https://www.youtube.com/embed/fUMqiEIkbBQ",
